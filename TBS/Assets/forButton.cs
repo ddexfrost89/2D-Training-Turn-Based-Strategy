83eed0d90aac30859eadfa0a5db14e96273b6e24
dexfrost89 <dexfrost89@gmail.com>
2015-07-05 21:40:33 +0300

    Button added
    Added a script for button(Unity 5.0.3)

---
+++ TBS/Assets/forButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class forButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void ButtonFunction() {  //код для кнопки сюда
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (0, Screen.height - 30, 75, 20), "Press me")) {
+			ButtonFunction();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
